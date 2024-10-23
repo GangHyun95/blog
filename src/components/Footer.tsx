173bee38e31cbc691863a8f3c667c73e98b183d1
허강현 <hgh6128@gmail.com>
2024-10-23 18:15:13 +0900

    Header 컴포넌트 UI 완성 및 라우트 설정

---
+++ src/components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+    return <p className='py-2 text-sm text-center border-t border-gray-200'>© 2024. hyun All rights reserved.</p>;
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import {
+    IoHomeOutline,
+    IoNewspaperOutline,
+} from 'react-icons/io5';
+import { FiUser } from 'react-icons/fi';
+import { TbMessageCircle } from 'react-icons/tb';
+
+export default function Header() {
+    const linkClass = 'flex items-center gap-1 hover-class';
+    return (
+        <header className='flex justify-between items-center p-4'>
+            <Link href='/'>
+                <h1 className='text-3xl font-semibold'>
+                    <span className='text-blue-600'>Hy</span>un
+                </h1>
+            </Link>
+            <nav className='flex gap-4'>
+                <Link href='/' className={linkClass}>
+                    <IoHomeOutline className='text-xl' />
+                    <span>Home</span>
+                </Link>
+                <Link href='/about' className={linkClass}>
+                    <FiUser className='text-xl' />
+                    <span>About</span>
+                </Link>
+                <Link href='/posts' className={linkClass}>
+                    <IoNewspaperOutline className='text-xl' />
+                    <span>Posts</span>
+                </Link>
+                <Link href='contact' className={linkClass}>
+                    <TbMessageCircle className='text-xl' />
+                    <span>Contact</span>
+                </Link>
+            </nav>
+        </header>
+    );
+}
