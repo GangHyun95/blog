f76fea8211e503af972ac33ede48bf014cbe7397
허강현 <hgh6128@gmail.com>
2024-11-05 15:33:53 +0900

    react-markdown 라이브러리 적용

---
+++ src/components/MarkdownViewer.tsx
@@ -0,0 +1,6 @@
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+
+export default function MarkdownViewer({ content }: { content: string }) {
+    return <Markdown className='prose lg:prose-xl' remarkPlugins={[remarkGfm]}>{content}</Markdown>;
+}
