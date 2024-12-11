d7fc1dd3d91cdd9aa1bc226fdeb9e13070deb8af
허강현 <hgh6128@gmail.com>
2024-12-11 19:17:56 +0900

    레이아웃 꺠짐 수정

--- src/components/layout/Container.tsx
+++ src/components/layout/Container.tsx
@@ -1,3 +1,3 @@
 export default function Container({ children }: { children: React.ReactNode }) {
-    return <div className='px-4 max-w-7xl mx-auto'>{children}</div>;
+    return <div className='px-4 max-w-7xl mx-auto w-full'>{children}</div>;
 }
